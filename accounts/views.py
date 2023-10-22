4368125911cba1b2fc4a152b28788506abc34934
Erin Yan <changyannnnnn@gmail.com>
2023-10-21 22:21:36 -0700

    attempt to add files in accounts, and changed auth related settings

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+
+class CheckEmailView(APIView):
+    permission_classes = (AllowAny, )
+
+    def post(self, request):
+        email = request.data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            return Response({"isNewUser": False})
+        else:
+            return Response({"isNewUser": True})
--- petsocialmediabackend/settings.py
+++ petsocialmediabackend/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -64,6 +65,8 @@ MIDDLEWARE = [
 ]
 
 # Authentication
+
+AUTH_USER_MODEL = 'accounts.UserAccount'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
