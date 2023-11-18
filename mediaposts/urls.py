3ba5bf68942f209fa574026446aa2a6ce102fd15
Erin Yan <changyannnnnn@gmail.com>
2023-11-18 11:35:19 -0800

    added delete post functions and api endpoints

--- mediaposts/urls.py
+++ mediaposts/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('create_post/', views.create_post_view, name='create_post'),
     path('post_media/<int:post_id>/<str:detail_level>/',
          views.get_post_media, name='get_post_media'),
+    path('delete_post/<int:post_id>/',
+         views.delete_post_view, name='delete_post'),
 ]
--- mediaposts/views.py
+++ mediaposts/views.py
@@ -1,3 +1,7 @@
+from django.db import transaction
+import boto3
+from urllib.parse import urlparse
+from .models import Post
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from django.shortcuts import get_object_or_404
@@ -199,3 +203,50 @@ def get_post_media(request, post_id, detail_level='overview'):
     }
 
     return Response(response_data)
+
+
+################## Post Deletion ####################
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_post_view(request, post_id):
+    try:
+        # Retrieve the post and verify that the request user is the owner
+        post = Post.objects.get(id=post_id, pet__user=request.user)
+    except Post.DoesNotExist:
+        return Response({'error': 'Post not found or not authorized to delete'}, status=404)
+
+    with transaction.atomic():
+        # Delete media files from Digital Ocean
+        for media in post.media.all():
+            delete_media_from_digital_ocean(media.image_url)
+            delete_media_from_digital_ocean(media.thumbnail_medium_url)
+            delete_media_from_digital_ocean(media.thumbnail_small_url)
+
+        post.delete()
+
+    return Response({'message': 'Post deleted successfully'}, status=200)
+
+
+def delete_media_from_digital_ocean(url):
+    if url:
+        parsed_url = urlparse(url)
+        path_parts = parsed_url.path.lstrip('/').split('/')
+        bucket_name = path_parts[0]
+        object_name = '/'.join(path_parts[1:])
+
+        # Set up boto3 client with Digital Ocean Spaces credentials
+        client = boto3.client('s3',
+                              region_name='sfo3',  # Replace with your region if different
+                              # Replace with your endpoint URL if different
+                              endpoint_url='https://sfo3.digitaloceanspaces.com',
+                              aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                              aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+
+        try:
+            client.delete_object(Bucket=bucket_name, Key=object_name)
+            print(f"Successfully deleted {object_name} from {bucket_name}")
+        except Exception as e:
+            print(
+                f"Failed to delete {object_name} from {bucket_name}. Error: {e}")
