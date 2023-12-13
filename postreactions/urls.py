ecf39b8feda0dbcaae502d73b2536db30c47a8e7
Erin Yan <changyannnnnn@gmail.com>
2023-12-12 19:19:37 -0800

    fixed pet id bug

--- postreactions/urls.py
+++ postreactions/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import check_like_status, get_like_count, get_likers_of_post, like_post, unlike_post
 
 urlpatterns = [
-    path('posts/<int:post_id>/like/<int:pet_profile_id>/',
+    path('posts/<int:post_id>/like/<str:pet_profile_id>/',
          like_post, name='like_post'),
-    path('posts/<int:post_id>/unlike/<int:pet_profile_id>/',
+    path('posts/<int:post_id>/unlike/<str:pet_profile_id>/',
          unlike_post, name='unlike_post'),
     path('posts/<int:post_id>/likecount/',
          get_like_count, name='get_like_count'),
