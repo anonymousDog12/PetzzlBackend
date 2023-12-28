9b48330e6ea0e91a9889c699bbfb7cea6b9fc268
Erin Yan <changyannnnnn@gmail.com>
2023-12-28 18:26:02 +0000

    changed debug true/false based on env

--- petsocialmediabackend/settings.py
+++ petsocialmediabackend/settings.py
@@ -29,8 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if ENV == DEV:
+    DEBUG = True
+else:  # PROD
+    DEBUG = False
 
 # Application definition
 
