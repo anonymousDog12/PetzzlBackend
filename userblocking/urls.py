1e4e0145c3595186470a2c24336949526f1822d7
Erin Yan <changyannnnnn@gmail.com>
2024-01-12 08:49:28 +0000

    Get blocked list

--- userblocking/urls.py
+++ userblocking/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('block/', views.block_user, name='block_user'),
     path('unblock/', views.unblock_user, name='unblock_user'),
+    path('blocked_profiles/', views.get_blocked_profiles,
+         name='get_blocked_profiles'),
 ]
